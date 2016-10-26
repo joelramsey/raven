d1cad8309a1857bd7fa1811967b2eee751cf29ba
jonathan.thompson <jonathan@urbaneinnovation.com>
2016-10-26 13:42:01 -0600

    Updated to SASS and cleaned up links a bit.

--- ui/src/app/sources/sources.component.ts
+++ ui/src/app/sources/sources.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sources',
   templateUrl: './sources.component.html',
-  styleUrls: ['./sources.component.css']
+  styleUrls: ['./sources.component.scss']
 })
 export class SourcesComponent implements OnInit {
 
