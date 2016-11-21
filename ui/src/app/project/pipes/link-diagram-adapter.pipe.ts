919b345b1e4d018f9a4bc4105c6e7ada8dd09220
jonathan.thompson <jonathan@urbaneinnovation.com>
2016-11-21 14:41:34 -0700

    Initial crack at link diagram adapter.

--- ui/src/app/project/pipes/link-diagram-adapter.pipe.ts
+++ ui/src/app/project/pipes/link-diagram-adapter.pipe.ts
@@ -1,7 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Source } from '../../shared/models/index';
 import { DataTableRow } from '../visualizations/data-table/data-table.interface';
-import { AlchemyConcept, AlchemyEntity, AlchemyTypedRelation } from '../../shared/models/record.interface';
+import {
+  AlchemyConcept, AlchemyEntity, AlchemyTypedRelation,
+  AlchemyArgumentEntity, AlchemyRelationArgument
+} from '../../shared/models/record.interface';
 import { LinkDiagramDatum } from '../visualizations/link-diagram/link-diagram-datum.interface';
 
 @Pipe({
@@ -25,6 +28,7 @@ export class LinkDiagramAdapterPipe implements PipeTransform {
       links: []
     };
     
+    let nodeIds: Array<number> = [];
     
     if (!value) {
       return result;
@@ -40,28 +44,46 @@ export class LinkDiagramAdapterPipe implements PipeTransform {
     
       if (source.record.result.typedRelations) {
         source.record.result.typedRelations.forEach((relation: AlchemyTypedRelation) => {
-          // rows.push({
-          //   name: relation.text,
-          //   type: 'Relation',
-          //   relevance: relation.relevance
-          // })
-        });
-      }
-    
-      if (source.record.result.entities) {
-        source.record.result.entities.forEach((entity: AlchemyEntity) => {
+
+          relation.arguments.forEach((argument: AlchemyRelationArgument) => {
+            
+            argument.entities.forEach((entity: AlchemyArgumentEntity) => {
+              
+              let id: number = parseInt(entity.id.substr(2));
+              
+              // If node doesn't exist, add it
+              //
+              if (nodeIds.indexOf(id) === -1) {
+                nodeIds.push(id);              
+                result.nodes.push({
+                  name: entity.text,
+                  group: id
+                });
+              }
+            });
+            
+          });
           
-          // rows.push({
-          //   name: entity.text,
-          //   type: entity.type,
-          //   sentiment: entity.sentiment.type
-          // })
+          // Create link - a bit magic number-y, but eso si que es
+          //
+          result.links.push({
+            source: parseInt(relation.arguments[0].entities[0].id.substr(2)),
+            target: parseInt(relation.arguments[1].entities[0].id.substr(2)),
+            value: Math.floor(parseFloat(relation.score) * 10)
+          });
+          
+          console.log(JSON.stringify({
+            source: parseInt(relation.arguments[0].entities[0].id.substr(2)),
+            target: parseInt(relation.arguments[1].entities[0].id.substr(2)),
+            value: Math.floor(parseFloat(relation.score) * 10)
+          }));
         });
       }
-    
+      
       return res;
     }, result);
-    
+
+    console.log(JSON.stringify(result.nodes));
     return result;
   }
 }
