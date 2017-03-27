e40202f42144d752f55284d45dd7553f8b225a8e
jonathan.thompson <jonathan@urbaneinnovation.com>
2017-03-26 23:42:33 -0600

    Added rudimentary card to display source metadata during citation editing.

--- client/src/app/project/citation-manager/citation-manager.module.ts
+++ client/src/app/project/citation-manager/citation-manager.module.ts
@@ -1,21 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MdButtonModule, MdTooltipModule, MdIconModule } from '@angular/material';
+import { MdButtonModule, MdCardModule, MdTooltipModule, MdIconModule } from '@angular/material';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { CitationManagerComponent } from './citation-manager.component';
 import { CitationListComponent } from './citation-list/citation-list.component';
 import { HasCitationPipe } from './pipes/has-citation.pipe';
 import { CitationEditorComponent } from './citation-editor/citation-editor.component';
-
+import { AccordionModule } from '../../shared/accordion/accordion.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MdButtonModule,
+    MdCardModule,
     MdTooltipModule,
     MdIconModule,
     NgxDatatableModule,
+    AccordionModule
   ],
   declarations: [
     CitationManagerComponent,
