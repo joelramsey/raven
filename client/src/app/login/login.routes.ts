d09054e3197294cf088df2a055bbdf70875de930
joelramsey <joel.d.ramsey@gmail.com>
2017-03-27 19:17:14 -0600

    more login and registration mods

--- client/src/app/login/login.routes.ts
+++ client/src/app/login/login.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 import { AntiAuthGuard } from '../shared/services/index';
+import { RegisterComponent } from '../register/register.component';
 
 export const LoginRoutes:Routes = [
   {
     path: 'login',
     canActivate: [AntiAuthGuard],
     component: LoginComponent
+  },
+  { path: 'register',
+    component: RegisterComponent
   }
+
 ];
