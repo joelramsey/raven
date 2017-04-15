32b52e3fc596ec12965c2ca0f2075783227e646e
joelramsey <joel.d.ramsey@gmail.com>
2017-04-15 13:30:05 -0600

    added back in the citations enpoint business

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 class CitationsController < ApplicationController
   before_action :set_citation, only: [:show, :update, :destroy]
 
@@ -13,6 +15,13 @@ class CitationsController < ApplicationController
   # GET /citations/1.json
   def show
     render json: @citation
+  end
+
+  # GET /cite
+  def cite
+    response = HTTParty.post("https://api.citation-api.com/2.1/rest/cite?#{params[:q]}")
+    puts response
+    render json: response.body
   end
 
   # POST /citations
@@ -44,13 +53,13 @@ class CitationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_citation
-      @citation = Citation.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_citation
+    @citation = Citation.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def citation_params
-      params.require(:citation).permit(:text, :record_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def citation_params
+    params.require(:citation).permit(:text, :record_id, :q)
+  end
 end
